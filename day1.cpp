635ccd790865249a8db4dd174386ae333b4aec8c
Abdul-rub <arub0419@gmail.com>
2023-10-02 23:19:51 +0530

    sum,subtract,multiply

---
+++ day1.cpp
@@ -0,0 +1,53 @@
+
+// Home Work 
+// Print number
+// 1)Two numbers are given, print their product
+// 2)Two numbers are given a and b, print a-b
+// 3)Print 6*3 in output, don’t print 18, just 6*3 should be in the output screen.
+// 4)If we have only 4 bits, How 3 and -6 will be written in 4 bits.
+// 5)If we have only 5 bits, How 13 and -16 will be written in 5 bits.
+
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    // int a;
+    // cout << "Enter a number : ";
+    // cin >> a;
+    // cout << "Value of a is " << a << endl;
+
+////////////////////////////// 1)Two numbers are given, print their product////////////////
+
+//  int a,b;
+//  a=10;
+//  b=2;
+//   int sum = a+b;
+//  cout << "Sum of  "<< a << " and " << b << " is "<< sum ;
+
+////////////////////////////// // 2)Two numbers are given a and b, print a-b ///////////////////
+
+// int a,b,value;
+
+// a=55;
+// b=30;
+// value = a-b;
+
+//  cout << "Subtraction of "<< a << " and " << b << " is "<< value ;
+
+///////// // 3)Print 6*3 in output, don’t print 18, just 6*3 should be in the output screen./////
+
+// int a,b,value;
+
+// a=5;
+// b=3;
+// value = a*b;
+
+//  cout << " Multiplication of "<< a << " and " << b << " is "<< value ;
+
+
+
+
+
+}
