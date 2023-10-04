ab20e773299df0f32d02691437b45686bc4a058a
Abdul-rub <arub0419@gmail.com>
2023-10-04 18:39:50 +0530

    day3_Pattern_printing_Basic_level

---
+++ D3 Pattern-I/D3_pattern_class.cpp
@@ -0,0 +1,153 @@
+#include<iostream>
+using namespace std;
+
+// 1) Print 
+// * * * * 
+// * * * *
+// * * * *
+// * * * *
+// int main(){
+//     int n,row,col;
+//    cout << "Enter a number : ";
+//    cin >> n;
+//    for(row=0;row<n;row++){
+//     for(col=0;col<n;col++){
+//         cout << "* ";
+//     }
+//     cout<<endl;
+//    }
+// }
+
+// 2) Print 
+// 10 10 10 10 10 
+// 10 10 10 10 10
+// 10 10 10 10 10
+// 10 10 10 10 10
+// 10 10 10 10 10
+// int main(){
+//     int n,row,col;
+//    cout << "Enter a number : ";
+//    cin >> n;
+//    for(row=0;row<n;row++){
+//     for(col=0;col<n;col++){
+//         cout << "10 ";
+//     }
+//     cout<<endl;
+//    }
+// }
+
+
+// 3) Print 
+// 1 1 1 1 1 
+// 2 2 2 2 2
+// 3 3 3 3 3
+// 4 4 4 4 4
+// 5 5 5 5 5
+
+// int main (){
+//     int n,row,col;
+//     cout << "Enter a number : ";
+//     cin >> n ;
+//     for(row=1;row<=n;row++){
+//     for(col=1;col<=n;col++){
+//         cout << row << " " ;
+//     }
+//     cout << endl;
+//     }
+// }
+
+// 4) Print 
+// 1 2 3 4 5 
+// 1 2 3 4 5
+// 1 2 3 4 5
+// 1 2 3 4 5
+// 1 2 3 4 5
+
+// int main (){
+//     int n,row,col;
+//     cout << "Enter a number : ";
+//     cin >> n ;
+//     for(row=1;row<=n;row++){
+//     for(col=1;col<=n;col++){
+//         cout << col << " " ;
+//     }
+//     cout << endl;
+//     }
+// }
+
+// 5) Print 
+// 5 4 3 2 1 
+// 5 4 3 2 1
+// 5 4 3 2 1
+// 5 4 3 2 1
+// 5 4 3 2 1
+
+// int main(){
+//     int n,row,col;
+
+//     cout << "Enter a number : ";
+//     cin >> n;
+//     for(row=1; row<=n;row++){
+//         for(col=n;col>=1;col--){
+//             cout << col <<" ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+// 6) Print 
+// a a a a a 
+// b b b b b
+// c c c c c
+// d d d d d
+// e e e e e
+
+// int main(){
+// char row ;
+// char col ;
+//   for(row=1; row<=5;row++){
+//     char name = 'a'+row-1;
+//     for(col=1; col<=5;col++){
+//         cout << name <<" " ;
+//     }
+//     cout << endl;
+// }
+// }
+
+// 7) Print 
+// a b c d e 
+// a b c d e
+// a b c d e
+// a b c d e
+// a b c d e
+
+// int main(){
+//     int row;
+//     char col;
+//     for(row=1;row<=5;row++){
+//         for(col='a'; col <='e';col++){
+//             cout << col <<" ";
+//         }
+//         cout <<endl;
+//     }
+// }
+
+// 8 ) Print 
+// 1 2 3 4 5 
+// 6 7 8 9 10
+// 11 12 13 14 15
+// 16 17 18 19 20
+// 21 22 23 24 25
+
+// int main(){
+// int row,col,n,count;
+// n=5;
+// count =1;
+// for(row=1;row<=n;row++){
+//    for(col=1;col<=n;col++){
+//     cout << count << " ";
+//     count++;
+//    }
+//    cout << endl;
+// }
+// }
---
+++ D3 Pattern-I/D3_pattern_homework.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+using namespace std;
+
+// 1) First Pattern:
+// 4 4 4 4 4 4
+// 4 4 4 4 4 4
+// 4 4 4 4 4 4
+// 4 4 4 4 4 4
+// 4 4 4 4 4 4
+
+
+// int main(){
+//  int row,col,n;
+//  n=5;
+//  for(row=1;row<=n;row++){
+//     for(col=1;col<=6;col++){
+//         cout << 4<<" " ;
+//     }
+//     cout << endl;
+//  }
+// }
+
+// 2) Second Pattern:
+// 1 4 9 16 25
+// 1 4 9 16 25
+// 1 4 9 16 25
+// 1 4 9 16 25
+// 1 4 9 16 25
+// 1 4 9 16 25
+
+// int main(){
+//     int row,col;
+//     for(row =1;row<=5;row++){
+//         for(col=1;col<=5;col++){
+//             cout << col*col<<" " ;
+//         }
+//         cout << endl;
+//     }
+// }
+
+// 3) Third Pattern:
+// 1 8 27 64 125 216
+// 1 8 27 64 125 216
+// 1 8 27 64 125 216
+// 1 8 27 64 125 216
+// 1 8 27 64 125 216
+
+
+// int main(){
+//     int row,col;
+//     for(row=0;row<5;row++){
+//         for(col=1;col<=6;col++){
+//             cout << col*col*col<< " ";
+//         }
+//         cout <<endl;
+//     }
+// }
+
+
+// 4) Fourth Pattern:
+// F G H I J K
+// F G H I J K
+// F G H I J K
+// F G H I J K
+// F G H I J K
+
+// int main(){
+//     int row;
+//     char col;
+//     for(row=1;row<=5;row++){
+//         for(col='F';col<='K';col++){
+//             cout << col <<" ";
+//         }
+//         cout <<endl;
+//     }
+// }
